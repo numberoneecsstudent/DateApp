935f006301b3036685d3fa351c76ad9403d39ca7
numberoneecsstudent <32531002+numberoneecsstudent@users.noreply.github.com>
2019-07-02 13:57:26 -0700

    started the security

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IauthRepository _repo;
+        public AuthController (IauthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto)
+        {
+            // validate request
+
+            username = username.ToLower();
+
+            if (await _repo.UserExists(usermane))
+                return BadRequest("Username already exists");
+            
+            var userToCreate = new User
+            {
+                username = username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, password);
+
+            return StatusCode(201);
+        }
+    }
+}
